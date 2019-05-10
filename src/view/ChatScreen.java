4d33b415f1992a9b2072ecbc4a91305966e91765
Niels <cutebou@hotmail.nl>
2019-05-10 15:18:55 +0200

    added HBox

--- src/view/ChatScreen.java
+++ src/view/ChatScreen.java
@@ -1,19 +1,28 @@
 package view;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class ChatScreen extends BorderPane {
     private Button send;
     private TextField input;
+    private HBox chat;
+    private HBox chatInput;
 
     public ChatScreen(){
         send = new Button("Send");
+        input = new TextField();
+        chat = new HBox();
 
         setMinSize(200, 200);
         setPrefSize(1000, 1000);
 
-        setRight(send);
+        chat.getChildren().addAll(input, send);
+
+        this.setBottom(chat);
+        this.setAlignment(chat, Pos.BOTTOM_CENTER);
     }
 }
